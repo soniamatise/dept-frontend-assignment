b16a7b3223043bdc8b16a0e1edaa7d26b1fb350a
Sonia <sonia@matise.nl>
2019-03-14 20:56:27 +0100

    add fonts

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,28 @@ export default {
 	components: {
 		caseBlock,
 		clients
+	},
+	data () {
+        return {
+            clients: [
+                { url: require('~/static/images/clients/ABN.png')},
+                { url: require('~/static/images/clients/Adidas.png')},
+                { url: require('~/static/images/clients/KLM.png')},
+                { url: require('~/static/images/clients/Microsoft.png')},
+                { url: require('~/static/images/clients/Mona.png')},
+                { url: require('~/static/images/clients/Nivea.png')},
+                { url: require('~/static/images/clients/NN.png')},
+                { url: require('~/static/images/clients/Oxxio.png')},
+				{ url: require('~/static/images/clients/Pathe.png')},
+				{ url: require('~/static/images/clients/Tomtom.png')},
+				{ url: require('~/static/images/clients/Transavia.png')},
+				{ url: require('~/static/images/clients/Triumph.png')},
+				{ url: require('~/static/images/clients/Unilever.png')},
+				{ url: require('~/static/images/clients/Walibi.png')},
+				{ url: require('~/static/images/clients/Zalando.png')},
+				{ url: require('~/static/images/clients/Ziggo.png')},
+            ]
+		}
 	}
 };
 </script>
