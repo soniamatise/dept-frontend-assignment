06be95ee39bffdf3ca3c8a7d4399bc52cc7a4edc
Sonia <sonia@matise.nl>
2019-03-14 17:33:45 +0100

    rename caseblock

---
+++ components/elements/case-block.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="row center small-full medium-full large-22 work-together">
+		<div class="column">
+			<div class="work-together__inner work-together__inner--text">
+				<slot />
+			</div>
+			<div class="work-together__inner work-together__inner--image">
+				<img :src="asset" />
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	components: {
+	},
+	props: ['asset']
+};
+</script>
+
+<style lang="scss">
+@import '~tools';
+</style>
